6856e76e210b1c6042e8f962575f3bae80625766
Aliciea D Suechin <adr330@nyu.edu>
2019-06-20 02:11:52 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,13 +1,28 @@
 def get_first_name_of_season_winner(data, season)
   # code here
+  winner = 0 
+  data[season.to_sym].each do |name|
+    name.each do |first_name|
+      if first_name == "Winner"
+        winner = first_name.split(" ").first
+      end
+    end
+  end
+  winner
 end
 
 def get_contestant_name(data, occupation)
   # code here
+  data.values
+      .flatten
+      .find { |person| person[:occupation] == occupation}
+      &.dig(:name)
+    end
 end
 
 def count_contestants_by_hometown(data, hometown)
   # code here
+  
 end
 
 def get_occupation(data, hometown)
