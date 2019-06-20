07029422f64d72e895ecc041c7d889e0ce2af2c0
Aliciea D Suechin <adr330@nyu.edu>
2019-06-20 02:06:18 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2,7 +2,6 @@ def get_first_name_of_season_winner(data, season)
   # code here
   data[season.first_name].find do |person| 
     person[:status] == "Winner" 
-    &.dig(:name)
 end
 
 def get_contestant_name(data, occupation)
